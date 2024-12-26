0f51d9871ebc048067a9687ad0a77d8e7fb40bb1
Kevin Zhang <kevzhang2022@gmail.com>
2024-12-25 21:50:58 -0600

    WIP: test packaged scoring logic

--- resume_scanner/parsing/initial_parsing.py
+++ resume_scanner/parsing/initial_parsing.py
@@ -1,24 +1,24 @@
 from pdf2image import convert_from_path
 from pytesseract import pytesseract
-from pydantic import BaseModel, RootModel
 
 from ..utils.decode import decode_with_ollama
 from ..utils.extract_pdf_text import extract_pdf_text
-from .schemas import Resume
+from ..schemas.parsing_schemas import Resume
 
 with open("../config/prompts/parsing/initial_extraction.txt", "r") as file:
     INITIAL_EXTRACTION_TEMPLATE = file.read()
 
-def parse_resume_sections(resume_path: str) -> (BaseModel | RootModel):
+def parse_resume_sections(resume_path: str) -> Resume:
     resume = extract_pdf_text(resume_path)
     
-    # Unable to directly extract text from image-based PDF
+    # If unable to directly extract text from image-based PDF
     if not resume:
         pdf_images = convert_from_path(resume_path, dpi=300)
         resume = pytesseract.image_to_string(pdf_images[0])
     
-    parsed_sections = decode_with_ollama(
+    parsed_sections: Resume = decode_with_ollama(
         prompt=INITIAL_EXTRACTION_TEMPLATE.format(resume_text=resume),
         schema=Resume
     )
+    
     return parsed_sections
--- resume_scanner/parsing/parsing.py
+++ resume_scanner/parsing/parsing.py
@@ -1,9 +1,8 @@
-from pydantic import BaseModel, RootModel
-
 from .initial_parsing import parse_resume_sections
 from .section_info_parsing import parse_section_info
+from ..schemas.parsing_schemas import ResumeInfo
 
-def parse_resume(resume_path: str) -> (BaseModel | RootModel):
+def parse_resume(resume_path: str) -> ResumeInfo:
     parsed_sections = parse_resume_sections(resume_path)
     parsed_info = parse_section_info(parsed_sections)
     return parsed_info
